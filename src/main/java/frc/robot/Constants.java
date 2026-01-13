36bd8c3f8948e9be0c8798df733f49c6f9253d84
Mehvoy <snomthaler@gmail.com>
2026-01-12 18:15:44 -0800

    Made the rollers code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,7 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+  public static class HardwareConstants {
+    public static final int kRollersID = 0;
+  }
 }
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,9 +1,41 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
+    private final TalonFX rollers;
+
     public IntakeSubsystem() {
-        
+        rollers = new TalonFX(Constants.HardwareConstants.kRollersID);
     }
+
+    public void rollerConfig() {
+        TalonFXConfigurator rollersCfg = rollers.getConfigurator();
+
+        TalonFXConfiguration talonFXConfiguration = new TalonFXConfiguration();
+
+        talonFXConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+        rollersCfg.apply(talonFXConfiguration);
+    }
+
+    public Command spinRollers(double speed) {
+        return Commands.runOnce(
+            () -> {
+                rollers.set(speed);
+    });}
+
+    public Command stopRollers() {
+        return Commands.runOnce(
+            () -> {
+                rollers.set(0);
+    });}
 }
