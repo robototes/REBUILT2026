a255b8d68039e5b01bb7ddfcd12df9514732f791
jamesdooley4 <56771024+jamesdooley4@users.noreply.github.com>
2026-01-12 23:15:47 -0800

    Remove getInstance

--- src/main/java/frc/robot/test/AutomatedTestMain.java
+++ src/main/java/frc/robot/test/AutomatedTestMain.java
@@ -4,6 +4,6 @@ import edu.wpi.first.wpilibj.RobotBase;
 
 public final class AutomatedTestMain {
   public static void main(String[] args) {
-    RobotBase.startRobot(AutomatedTestRobot::getInstance);
+    RobotBase.startRobot(AutomatedTestRobot::new);
   }
 }
