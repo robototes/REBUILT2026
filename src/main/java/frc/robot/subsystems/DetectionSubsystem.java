e84bcef4d1eaa6c2250aa200344cf9cd8af73ac8
k <kreal.kyla@gmail.com>
2026-01-17 11:29:48 -0800

    added things

--- src/main/java/frc/robot/subsystems/DetectionSubsystem.java
+++ src/main/java/frc/robot/subsystems/DetectionSubsystem.java
@@ -4,6 +4,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Hardware;
 import frc.robot.util.LLCamera;
 import frc.robot.util.LimelightHelpers.RawDetection;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import frc.robot.util.BetterPoseEstimate;
 
 public class DetectionSubsystem extends SubsystemBase {
   private static final String LIMELIGHT_C = Hardware.LIMELIGHT_C;
@@ -36,7 +39,25 @@ public class DetectionSubsystem extends SubsystemBase {
     return B;
   }
 
-  public double getDistanceToTarget() {
-    return (double) Math.round(distance * 1000) / 1000;
+  public Pose2d getDistanceToTarget() {
+    Pose2d a = null;
+    return a;
+  }
+
+  public double getTargetRotation() {
+    return 0.0;
+  }
+
+  public void rotateRobot(double rotation) {}
+
+  public Pose3d getRobotPosition(LLCamera camera) {
+    BetterPoseEstimate estimate = camera.getBetterPoseEstimate();
+    if (estimate != null) {
+      Pose3d fieldPose3d = estimate.pose3d;
+      return fieldPose3d;
+    } else {
+      Pose3d nullPose = null;
+      return nullPose;
+    }
   }
 }
