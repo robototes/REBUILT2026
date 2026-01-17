1e21a2aa6feb7a6ec2ed1ca557a3ff31ba3120c1
RobototesProgrammers <194180343+RobototesProgrammers@users.noreply.github.com>
2026-01-17 09:56:30 -0800

    better flywheels

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -7,7 +7,8 @@ public class Hardware {
   // Swerve: 1-12
 
   // Alpha bot
-  public static final int FLYWHEEL_ID = 13;
+  public static final int FLYWHEEL_ONE_ID = 13;
+  public static final int FLYWHEEL_TWO_ID = 16;
   public static final int INTAKE_MOTOR_ID = 14;
   public static final int INDEX_MOTOR_ID = 15;
 }
