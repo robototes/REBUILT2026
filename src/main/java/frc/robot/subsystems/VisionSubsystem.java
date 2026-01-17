4783419b513d90c2215314da90cc5d88399c0953
koolpoolo <s-petersonv@bsd405.org>
2026-01-17 09:38:53 -0800

    Limelight localization (#6)
    * ported stuffover and got aut orient started
    
    * spotless implementation
    
    * some changes from gemini + getting rid of shortest distance thing
    
    * spotless
    
    * stuffs
    
    * told vision to update in robot periodic
    
    * sim thing i got maybe helps idk
    
    * got rid of shuffle board
    
    * spotless
    
    * removed phoenix 6
    
    * got rid of libgrapple
    
    * got rid of dead code
    
    * git ignored some elastic stuff
    
    * turned vision off
    
    * spotless
    
    * turned vision back on
    
    * fixed it i think
    
    * added another fix
    
    * spotless
    
    * added logging and some fixes
    
    * got rid of forceful exit
    
    * check if update is run
    
    * got rid of auto orient
    
    * spotless
    
    * make disable vision null again for final declaration
    
    * more
    
    * spotless
    
    * spotless
    
    * fix disable vision nt err
    
    * spotless
    
    * changed to boolean subscriber
    
    * spotless
    
    * check if disable fix exists
    
    * changed disable vision to boolean
    
    * spotless
    
    * got rid of logging checks
    
    * spotless
    
    * updated wpilib to 2026.2.1 which allowed me to add 2026 field for vision
    
    * changed get distance to via pose estimation bc that worked last year for pose filtering and that was really good last year
    
    * added comment for get distance
    
    * spotless
    
    * gemini review changes + pose for hubs and pose estimation distance instead of camera distance
    
    * got rid of inconsistent comment in drivebase wrapper
    
    * spotless
    
    * gemini change for updating disable vision button i got confused on
    
    * some changes
    
    * added helpful comment
    
    * get drive is now private
    
    * maybe fixed gitignore
    
    * made a json valid
    
    * updated to limelight v1.12
    
    * spotless
    
    * maybe fixed error with disable i think
    
    * made the checks better for better pose estimate
    
    * fixed something
    
    * i realized something
    
    * made better pose estimate to abort if length is less than 11
    
    * got rid of length check for poseentry
    
    ---------
    
    Co-authored-by: koolpoolo <181432562+koolpoolo@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,188 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.Vector;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.FieldObject2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Hardware;
+import frc.robot.util.BetterPoseEstimate;
+import frc.robot.util.LLCamera;
+import frc.robot.util.LimelightHelpers.RawFiducial;
+
+public class VisionSubsystem extends SubsystemBase {
+  // Limelight names must match your NT names
+
+  private static final String LIMELIGHT_B = Hardware.LIMELIGHT_B;
+  // hub pose blue X: 4.536m, Y: 4.053m
+  // hub pose red X: 11.950m, Y: 4.105m,
+  // Deviations
+  private static final Vector<N3> STANDARD_DEVS =
+      VecBuilder.fill(0.1, 0.1, Units.degreesToRadians(20));
+  private static final Vector<N3> DISTANCE_SC_STANDARD_DEVS =
+      VecBuilder.fill(1, 1, Units.degreesToRadians(50));
+
+  // AprilTag field layout for this year
+  private static final AprilTagFieldLayout fieldLayout =
+      AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded);
+
+  private final DrivebaseWrapper drivebaseWrapper;
+
+  private final Field2d robotField;
+  private final FieldObject2d rawVisionFieldObject;
+
+  private boolean disableVision;
+  private final LLCamera BCamera = new LLCamera(LIMELIGHT_B);
+
+  private final StructPublisher<Pose3d> fieldPose3dEntry =
+      NetworkTableInstance.getDefault()
+          .getStructTopic("vision/fieldPose3d", Pose3d.struct)
+          .publish();
+  private final StructPublisher<Pose3d> rawFieldPose3dEntryB =
+      NetworkTableInstance.getDefault()
+          .getStructTopic("vision/rawFieldPose3dLeft", Pose3d.struct)
+          .publish();
+
+  // state
+  private double lastTimestampSeconds = 0;
+  private Pose2d lastFieldPose = new Pose2d(-1, -1, new Rotation2d());
+  private double distance = 0;
+  private double tagAmbiguity = 0;
+
+  public VisionSubsystem(DrivebaseWrapper drivebaseWrapper) {
+    this.drivebaseWrapper = drivebaseWrapper;
+
+    robotField = new Field2d();
+    SmartDashboard.putData(robotField);
+    rawVisionFieldObject = robotField.getObject("RawVision");
+    SmartDashboard.putNumber("Last timestamp", getLastTimestampSeconds());
+    SmartDashboard.putNumber("Num targets", getNumTargets());
+    SmartDashboard.putNumber("april tag distance meters", getDistanceToTarget());
+    SmartDashboard.putNumber("time since last reading", getTimeSinceLastReading());
+    SmartDashboard.putNumber("tag ambiguity", getTagAmbiguity());
+    disableVision = SmartDashboard.getBoolean("Disable Vision", false);
+  }
+
+  public void update() {
+    // System.out.println("updating");
+    RawFiducial[] rawFiducialsB = BCamera.getRawFiducials();
+    // System.out.println("got raw fiducials");
+    if (rawFiducialsB != null) {
+      for (RawFiducial rf : rawFiducialsB) {
+        // System.out.println("processing raw fiducials");
+        processLimelight(BCamera, rawFieldPose3dEntryB, rf);
+      }
+    }
+  }
+
+  private void processLimelight(
+      LLCamera camera, StructPublisher<Pose3d> rawFieldPoseEntry, RawFiducial rf) {
+
+    disableVision = SmartDashboard.getBoolean("Disable Vision", false);
+    if (disableVision) {
+      return;
+    }
+
+    // System.out.println("processed a raw fiducial");
+    BetterPoseEstimate estimate = camera.getBetterPoseEstimate();
+    // System.out.println("got a pose estimate");
+
+    if (estimate != null) {
+      if (estimate.tagCount <= 0) {
+        // System.out.println("no tags");
+        return;
+      }
+
+      double timestampSeconds = estimate.timestampSeconds;
+      Pose3d fieldPose3d = estimate.pose3d;
+      var tagPose = fieldLayout.getTagPose(rf.id);
+      if (tagPose.isEmpty()) {
+        DriverStation.reportWarning(
+            "Vision: Received pose for tag ID " + rf.id + " which is not in the field layout.",
+            false);
+        return;
+      }
+      this.distance =
+          getDistanceToTargetViaPoseEstimation(fieldPose3d.toPose2d(), tagPose.get().toPose2d());
+      this.tagAmbiguity = rf.ambiguity;
+      boolean pose_bad = false;
+      rawFieldPoseEntry.set(fieldPose3d);
+      //   System.out.println("got new data");
+
+      if (!MathUtil.isNear(0, fieldPose3d.getZ(), 0.10)
+          || !MathUtil.isNear(0, fieldPose3d.getRotation().getX(), Units.degreesToRadians(8))
+          || !MathUtil.isNear(0, fieldPose3d.getRotation().getY(), Units.degreesToRadians(8))) {
+        pose_bad = true;
+        // System.out.println("pose bad");
+      }
+
+      if (!pose_bad) {
+
+        drivebaseWrapper.addVisionMeasurement(
+            fieldPose3d.toPose2d(),
+            timestampSeconds,
+            // start with STANDARD_DEVS, and for every
+            // meter of distance past 1 meter,
+            // add a distance standard dev
+            DISTANCE_SC_STANDARD_DEVS.times(Math.max(0, this.distance - 1)).plus(STANDARD_DEVS));
+        robotField.setRobotPose(drivebaseWrapper.getEstimatedPosition());
+        // System.out.println("put pose in");
+      }
+      if (timestampSeconds > lastTimestampSeconds) {
+        if (!pose_bad) {
+          fieldPose3dEntry.set(fieldPose3d);
+          lastFieldPose = fieldPose3d.toPose2d();
+          rawVisionFieldObject.setPose(lastFieldPose);
+          //   System.out.println("updated pose");
+        }
+        lastTimestampSeconds = timestampSeconds;
+        // System.out.println("updated time");
+      }
+    }
+  }
+
+  public int getNumTargets() {
+    int B = BCamera.getNumTargets();
+    return B;
+  }
+
+  public double getLastTimestampSeconds() {
+    return lastTimestampSeconds;
+  }
+
+  public double getTimeSinceLastReading() {
+    return Timer.getFPGATimestamp() - lastTimestampSeconds;
+  }
+
+  public double getDistanceToTarget() {
+    return (double) Math.round(distance * 1000) / 1000;
+  }
+
+  public double getDistanceToTargetViaPoseEstimation(Pose2d yourPose, Pose2d targetPose) {
+    double distance =
+        Math.hypot(targetPose.getX() - yourPose.getX(), targetPose.getY() - yourPose.getY());
+    // 0.1 millimeter
+    return (double) Math.round(distance * 10000) / 10000;
+  }
+
+  public double getTagAmbiguity() {
+    return tagAmbiguity;
+  }
+
+  public boolean getDisableVision() {
+    return disableVision;
+  }
+}
