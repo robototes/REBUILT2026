fbb1a11bd2a03bc85b8ed07e07b1ec2e4544ced1
Fatblabs <stuffducky@gmail.com>
2026-01-19 11:50:45 -0800

    Added cyclable states: IDLE and MANUAL, where idle just does nothing while manual means the driver can move the turret to their heart's desire. Wrapped everything into a command, so that the entire subsystem is only reliant on cycling states rather than relying on periodic().

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -14,10 +14,11 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Hardware;
+import java.util.function.DoubleSupplier;
 
 public class TurretSubsystem extends SubsystemBase {
   // ------ VARIABLES ------//
@@ -26,8 +27,8 @@ public class TurretSubsystem extends SubsystemBase {
 
   private final int GEAR_RATIO = 9000; // It's over 9000
   private final double MAXIMUM_DERIVATIVE = 0.3;
-  private final int TURRET_MIN = -90; // In Degrees
-  private final int TURRET_MAX = 90; // In Degrees
+  private final double TURRET_MIN = -90.0; // In Degrees
+  private final double TURRET_MAX = 90.0; // In Degrees
 
   private static final double HUB_X_INCHES = 182.11;
   private static final double HUB_Y_INCHES = 158.84;
@@ -39,8 +40,20 @@ public class TurretSubsystem extends SubsystemBase {
   // ----- HARDWARE OBJECTS ----- //
   private final SwerveDrivetrain m_driveTrain;
 
+  // --- STATES ---- //
+  public static enum turretState {
+    MANUAL(),
+    AUTO();
+
+    public turretState next() {
+      turretState[] states = values();
+      return states[((this.ordinal() + 1) % states.length)];
+    }
+  }
+
   // PLACEHOLDER VALUE. This will probably be handled elsewhere
   private boolean readyToShoot = false;
+  private turretState currentState = turretState.AUTO;
 
   public TurretSubsystem(SwerveDrivetrain drivetrain) {
     // --- MOTOR SETUP ---//
@@ -49,16 +62,13 @@ public class TurretSubsystem extends SubsystemBase {
     configureMotors();
     // --- SET DRIVETRAIN --- //
     this.m_driveTrain = drivetrain;
-
-    // --- SCHEDULE AUTO ZERO --- //
-    CommandScheduler.getInstance().schedule(autoZeroCommand());
   }
 
   private void moveMotor(double targetDegrees) {
     motor1.setControl(request.withPosition(Units.degreesToRotations(targetDegrees)));
   }
 
-  private double calculateRotations() {
+  private double calculateTargetDegrees() {
     // 158.84 Inches in the Y Direction
     // 182.11 inches in the X Direction
 
@@ -87,46 +97,92 @@ public class TurretSubsystem extends SubsystemBase {
     return turretDegrees;
   }
 
-  public Command autoZeroCommand() {
-    final double[] previousTimeStamp = new double[1];
-    final double[] previousCurrent = new double[1];
-    final int[] hits = new int[1];
-    return new FunctionalCommand(
-        () -> {
-          previousTimeStamp[0] = Timer.getTimestamp();
-          previousCurrent[0] = motor1.getTorqueCurrent(true).getValueAsDouble();
-          hits[0] = 0;
-          motor1.setControl(new VoltageOut(0.3));
-        },
-        () -> {
-          double now = Timer.getTimestamp();
-          double currentNow = motor1.getTorqueCurrent(true).getValueAsDouble();
-          double currentDT = now - previousTimeStamp[0];
-          if (currentDT < MIN_DT) {
-            return;
-          }
-          boolean isDerivativeHigh =
-              (currentNow - previousCurrent[0]) / (now - previousTimeStamp[0])
-                  >= MAXIMUM_DERIVATIVE;
-          boolean stopped = Math.abs(motor1.getVelocity().getValueAsDouble()) <= MIN_VELOCITY;
-          if (isDerivativeHigh && stopped) {
-            hits[0]++;
-          } else {
+  // ----- PUBLIC METHODS ----- //
+
+  public Command autoZeroCommand(boolean runAutoZeroRoutine) {
+    if (runAutoZeroRoutine) {
+      final double[] previousTimeStamp = new double[1];
+      final double[] previousCurrent = new double[1];
+      final int[] hits = new int[1];
+      return new FunctionalCommand(
+          () -> {
+            previousTimeStamp[0] = Timer.getTimestamp();
+            previousCurrent[0] = motor1.getTorqueCurrent(true).getValueAsDouble();
             hits[0] = 0;
+            motor1.setControl(new VoltageOut(0.3));
+          },
+          () -> {
+            double now = Timer.getTimestamp();
+            double currentNow = motor1.getTorqueCurrent(true).getValueAsDouble();
+            double currentDT = now - previousTimeStamp[0];
+            if (currentDT < MIN_DT) {
+              return;
+            }
+            boolean isDerivativeHigh =
+                (currentNow - previousCurrent[0]) / (now - previousTimeStamp[0])
+                    >= MAXIMUM_DERIVATIVE;
+            boolean stopped = Math.abs(motor1.getVelocity().getValueAsDouble()) <= MIN_VELOCITY;
+            if (isDerivativeHigh && stopped) {
+              hits[0]++;
+            } else {
+              hits[0] = 0;
+            }
+            previousTimeStamp[0] = now;
+            previousCurrent[0] = currentNow;
+          },
+          (Boolean interrupted) -> {
+            motor1.setControl(new VoltageOut(0));
+            if (!interrupted) {
+              motor1.setPosition(Units.degreesToRotations(TURRET_MAX + 0.5));
+              motor1.setControl(request.withPosition(Units.degreesToRotations(TURRET_MAX)));
+              readyToShoot = true;
+            }
+          },
+          () -> (hits[0] >= MIN_HITS),
+          TurretSubsystem.this);
+    }
+    motor1.setPosition(Units.degreesToRotations(TURRET_MAX + 0.5));
+    readyToShoot = true;
+    return Commands.none();
+  }
+
+  public Command autoAimCommand(DoubleSupplier xJoystick) {
+    return run(() -> {
+          if (readyToShoot) {
+            if (currentState == turretState.AUTO) {
+              moveMotor(calculateTargetDegrees());
+            } else {
+              manualMove(xJoystick);
+            }
+          } else {
+            stop();
           }
-          previousTimeStamp[0] = now;
-          previousCurrent[0] = currentNow;
-        },
-        (Boolean interrupted) -> {
-          motor1.setControl(new VoltageOut(0));
-          if (!interrupted) {
-            motor1.setPosition(Units.degreesToRotations(91));
-            motor1.setControl(request.withPosition(Units.degreesToRotations(90)));
-            readyToShoot = true;
-          }
-        },
-        () -> (hits[0] >= MIN_HITS),
-        TurretSubsystem.this);
+        })
+        .finallyDo(interrupted -> stop());
+  }
+
+  public void switchState() {
+    currentState = currentState.next();
+  }
+
+  public void manualMove(DoubleSupplier joystick) {
+    double deg = motor1.getPosition().getValueAsDouble() * 360.0;
+    double cmd = MathUtil.applyDeadband(joystick.getAsDouble(), 0.10);
+    double volts = cmd * 2.0;
+
+    if (deg >= TURRET_MAX && volts > 0) {
+      stop();
+      return;
+    } // trying to go further +
+    if (deg <= TURRET_MIN && volts < 0) {
+      stop();
+      return;
+    } // trying to go further -
+    motor1.setControl(new VoltageOut(volts));
+  }
+
+  public void stop() {
+    motor1.setControl(new VoltageOut(0));
   }
 
   private void configureMotors() {
@@ -158,9 +214,7 @@ public class TurretSubsystem extends SubsystemBase {
   // PERIODIC FUNCTIONS
   @Override
   public void periodic() {
-    if (readyToShoot) {
-      moveMotor(calculateRotations());
-    }
+
     super.periodic();
   }
 
