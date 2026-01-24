ae65e3fda21b934fe2cb3420779922eed8c6d6ec
Jetblackdragon <64041077+Jetblackdragon@users.noreply.github.com>
2026-01-24 13:10:16 -0800

    Removed Swerve ids from hardware constants

--- src/main/java/frc/robot/Hardware.java
+++ src/main/java/frc/robot/Hardware.java
@@ -11,25 +11,7 @@ public class Hardware {
 
   public static final int PDH_ID = 1;
   // Swerve: 1-12
-  // Front Left
-  private static final int kFrontLeftDriveMotorId = 1;
-  private static final int kFrontLeftSteerMotorId = 2;
-  private static final int kFrontLeftEncoderId = 3;
-
-  // Front Right
-  private static final int kFrontRightDriveMotorId = 4;
-  private static final int kFrontRightSteerMotorId = 5;
-  private static final int kFrontRightEncoderId = 6;
-
-  // Back Left
-  private static final int kBackLeftDriveMotorId = 7;
-  private static final int kBackLeftSteerMotorId = 8;
-  private static final int kBackLeftEncoderId = 9;
-
-  // Back Right
-  private static final int kBackRightDriveMotorId = 10;
-  private static final int kBackRightSteerMotorId = 11;
-  private static final int kBackRightEncoderId = 12;
+  // Check CompTunerConstants
 
   // Vision
   // TODO: Use more descriptive names for Limelights once their positions are known.
