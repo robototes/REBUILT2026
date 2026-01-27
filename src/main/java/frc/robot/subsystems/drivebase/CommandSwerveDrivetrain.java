95b3479b885bb8e27abbccff342d6c9258dea47b
Jetblackdragon <64041077+Jetblackdragon@users.noreply.github.com>
2026-01-26 18:17:57 -0800

    Refactoring if block

--- src/main/java/frc/robot/subsystems/drivebase/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/drivebase/CommandSwerveDrivetrain.java
@@ -211,17 +211,6 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
      * Otherwise, only check and apply the operator perspective if the DS is disabled.
      * This ensures driving behavior doesn't change until an explicit disable event occurs during testing.
      */
-    if (!m_hasAppliedOperatorPerspective || DriverStation.isDisabled()) {
-      DriverStation.getAlliance()
-          .ifPresent(
-              allianceColor -> {
-                setOperatorPerspectiveForward(
-                    allianceColor == Alliance.Red
-                        ? kRedAlliancePerspectiveRotation
-                        : kBlueAlliancePerspectiveRotation);
-                m_hasAppliedOperatorPerspective = true;
-              });
-    }
     if (RobotBase.isSimulation()) {
       DriverStation.getAlliance()
           .ifPresent(
@@ -230,6 +219,16 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
                     alliance == DriverStation.Alliance.Blue
                         ? kBlueAlliancePerspectiveRotation
                         : kRedAlliancePerspectiveRotation);
+              });
+    } else if (!m_hasAppliedOperatorPerspective || DriverStation.isDisabled()) {
+      DriverStation.getAlliance()
+          .ifPresent(
+              allianceColor -> {
+                setOperatorPerspectiveForward(
+                    allianceColor == Alliance.Red
+                        ? kRedAlliancePerspectiveRotation
+                        : kBlueAlliancePerspectiveRotation);
+                m_hasAppliedOperatorPerspective = true;
               });
     }
   }
