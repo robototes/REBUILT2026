e6150f6a0cdaa53b906c6e25a73b41dada16184b
k <kreal.kyla@gmail.com>
2026-01-27 18:00:25 -0800

    Update Controls.java

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -57,19 +57,7 @@ public class Controls {
     configureFeederBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureFeederBindings() {
-    // start motor when robot starts
-    m_serializerSubsystem.startMotor();
-
     // start serializer motor
     driverController.a().onTrue(m_serializerSubsystem.startMotor());
 
