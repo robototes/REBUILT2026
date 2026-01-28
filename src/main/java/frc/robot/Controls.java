72f761488c441eec187a5c93bbbe7ddaeaa3704d
k <kreal.kyla@gmail.com>
2026-01-27 17:44:28 -0800

    Update Controls.java

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -55,15 +55,6 @@ public class Controls {
     configureDrivebaseBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   public Command setRumble(RumbleType type, double value) {
     return Commands.runOnce(
         () -> {
