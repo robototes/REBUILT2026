e442ec4bc98afb53b7b62e710654e55bf45e9991
SnappleRamen <130863864+SnappleRamen@users.noreply.github.com>
2026-02-06 10:16:57 -0800

    improvements

--- src/main/java/frc/robot/subsystems/auto/AutoLogic.java
+++ src/main/java/frc/robot/subsystems/auto/AutoLogic.java
@@ -28,12 +28,12 @@ public class AutoLogic {
 
   public enum StartPosition {
     LEFT_TRENCH(
-        "Left Trench", new Pose2d(4.014, 7.582, new Rotation2d(Units.degreesToRadians(90)))),
+        "Left Trench", new Pose2d(4.014, 7.465, new Rotation2d(Units.degreesToRadians(90)))),
     LEFT_BUMP("Left Bump", new Pose2d(3.664, 5.411, new Rotation2d(Units.degreesToRadians(90)))),
     CENTER("Center", new Pose2d(3.595, 4.008, new Rotation2d(Units.degreesToRadians(90)))),
     RIGHT_BUMP("Right Bump", new Pose2d(3.638, 2.322, new Rotation2d(Units.degreesToRadians(-90)))),
     RIGHT_TRENCH(
-        "Right Trench", new Pose2d(3.641, 0.478, new Rotation2d(Units.degreesToRadians(-90)))),
+        "Right Trench", new Pose2d(3.641, 0.5, new Rotation2d(Units.degreesToRadians(-90)))),
     MISC("Misc", null);
 
     final String title;
