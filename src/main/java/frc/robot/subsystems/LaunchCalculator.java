eefa5cdd9d009c10dc2de4d87d4bcb004f85bbd4
Roxie222 <s-furlongan@bsd405.org>
2026-02-14 12:50:14 -0800

    created LunchCalculator

---
+++ src/main/java/frc/robot/subsystems/LaunchCalculator.java
@@ -0,0 +1,180 @@
+package frc.robot.subsystems;
+
+public class LaunchCalculator {
+    import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Twist2d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
+import edu.wpi.first.math.interpolation.InverseInterpolator;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import lombok.experimental.ExtensionMethod;
+import org.littletonrobotics.frc2026.Constants;
+import org.littletonrobotics.frc2026.FieldConstants;
+import org.littletonrobotics.frc2026.RobotState;
+import org.littletonrobotics.frc2026.util.geometry.AllianceFlipUtil;
+import org.littletonrobotics.frc2026.util.geometry.GeomUtil;
+import org.littletonrobotics.junction.Logger;
+
+@ExtensionMethod({GeomUtil.class})
+public class LaunchCalculator {
+  private static LaunchCalculator instance;
+
+  private final LinearFilter turretAngleFilter =
+      LinearFilter.movingAverage((int) (0.1 / Constants.loopPeriodSecs));
+  private final LinearFilter hoodAngleFilter =
+      LinearFilter.movingAverage((int) (0.1 / Constants.loopPeriodSecs));
+
+  private Rotation2d lastTurretAngle;
+  private double lastHoodAngle;
+  private Rotation2d turretAngle;
+  private double hoodAngle = Double.NaN;
+  private double turretVelocity;
+  private double hoodVelocity;
+
+  public static LaunchCalculator getInstance() {
+    if (instance == null) instance = new LaunchCalculator();
+    return instance;
+  }
+
+  public record LaunchingParameters(
+      boolean isValid,
+      Rotation2d turretAngle,
+      double turretVelocity,
+      double hoodAngle,
+      double hoodVelocity,
+      double flywheelSpeed) {}
+
+  // Cache parameters
+  private LaunchingParameters latestParameters = null;
+
+  private static double minDistance;
+  private static double maxDistance;
+  private static double phaseDelay;
+  private static final InterpolatingTreeMap<Double, Rotation2d> launchHoodAngleMap =
+      new InterpolatingTreeMap<>(InverseInterpolator.forDouble(), Rotation2d::interpolate);
+  private static final InterpolatingDoubleTreeMap launchFlywheelSpeedMap =
+      new InterpolatingDoubleTreeMap();
+  private static final InterpolatingDoubleTreeMap timeOfFlightMap =
+      new InterpolatingDoubleTreeMap();
+
+  static {
+    minDistance = 1.34;
+    maxDistance = 5.60;
+    phaseDelay = 0.03;
+
+    launchHoodAngleMap.put(1.34, Rotation2d.fromDegrees(19.0));
+    launchHoodAngleMap.put(1.78, Rotation2d.fromDegrees(19.0));
+    launchHoodAngleMap.put(2.17, Rotation2d.fromDegrees(24.0));
+    launchHoodAngleMap.put(2.81, Rotation2d.fromDegrees(27.0));
+    launchHoodAngleMap.put(3.82, Rotation2d.fromDegrees(29.0));
+    launchHoodAngleMap.put(4.09, Rotation2d.fromDegrees(30.0));
+    launchHoodAngleMap.put(4.40, Rotation2d.fromDegrees(31.0));
+    launchHoodAngleMap.put(4.77, Rotation2d.fromDegrees(32.0));
+    launchHoodAngleMap.put(5.57, Rotation2d.fromDegrees(32.0));
+    launchHoodAngleMap.put(5.60, Rotation2d.fromDegrees(35.0));
+
+    launchFlywheelSpeedMap.put(1.34, 210.0);
+    launchFlywheelSpeedMap.put(1.78, 220.0);
+    launchFlywheelSpeedMap.put(2.17, 220.0);
+    launchFlywheelSpeedMap.put(2.81, 230.0);
+    launchFlywheelSpeedMap.put(3.82, 250.0);
+    launchFlywheelSpeedMap.put(4.09, 255.0);
+    launchFlywheelSpeedMap.put(4.40, 260.0);
+    launchFlywheelSpeedMap.put(4.77, 265.0);
+    launchFlywheelSpeedMap.put(5.57, 275.0);
+    launchFlywheelSpeedMap.put(5.60, 290.0);
+
+    timeOfFlightMap.put(5.68, 1.16);
+    timeOfFlightMap.put(4.55, 1.12);
+    timeOfFlightMap.put(3.15, 1.11);
+    timeOfFlightMap.put(1.88, 1.09);
+    timeOfFlightMap.put(1.38, 0.90);
+  }
+
+  public LaunchingParameters getParameters() {
+    if (latestParameters != null) {
+      return latestParameters;
+    }
+
+    // Calculate estimated pose while accounting for phase delay
+    Pose2d estimatedPose = RobotState.getInstance().getEstimatedPose();
+    ChassisSpeeds robotRelativeVelocity = RobotState.getInstance().getRobotVelocity();
+    estimatedPose =
+        estimatedPose.exp(
+            new Twist2d(
+                robotRelativeVelocity.vxMetersPerSecond * phaseDelay,
+                robotRelativeVelocity.vyMetersPerSecond * phaseDelay,
+                robotRelativeVelocity.omegaRadiansPerSecond * phaseDelay));
+
+    // Calculate distance from turret to target
+    Translation2d target =
+        AllianceFlipUtil.apply(FieldConstants.Hub.topCenterPoint.toTranslation2d());
+    Pose2d turretPosition = estimatedPose.transformBy(robotToTurret.toTransform2d());
+    double turretToTargetDistance = target.getDistance(turretPosition.getTranslation());
+
+    // Calculate field relative turret velocity
+    ChassisSpeeds robotVelocity = RobotState.getInstance().getFieldVelocity();
+    double robotAngle = estimatedPose.getRotation().getRadians();
+    double turretVelocityX =
+        robotVelocity.vxMetersPerSecond
+            + robotVelocity.omegaRadiansPerSecond
+                * (robotToTurret.getY() * Math.cos(robotAngle)
+                    - robotToTurret.getX() * Math.sin(robotAngle));
+    double turretVelocityY =
+        robotVelocity.vyMetersPerSecond
+            + robotVelocity.omegaRadiansPerSecond
+                * (robotToTurret.getX() * Math.cos(robotAngle)
+                    - robotToTurret.getY() * Math.sin(robotAngle));
+
+    // Account for imparted velocity by robot (turret) to offset
+    double timeOfFlight;
+    Pose2d lookaheadPose = turretPosition;
+    double lookaheadTurretToTargetDistance = turretToTargetDistance;
+    for (int i = 0; i < 20; i++) {
+      timeOfFlight = timeOfFlightMap.get(lookaheadTurretToTargetDistance);
+      double offsetX = turretVelocityX * timeOfFlight;
+      double offsetY = turretVelocityY * timeOfFlight;
+      lookaheadPose =
+          new Pose2d(
+              turretPosition.getTranslation().plus(new Translation2d(offsetX, offsetY)),
+              turretPosition.getRotation());
+      lookaheadTurretToTargetDistance = target.getDistance(lookaheadPose.getTranslation());
+    }
+
+    // Calculate parameters accounted for imparted velocity
+    turretAngle = target.minus(lookaheadPose.getTranslation()).getAngle();
+    hoodAngle = launchHoodAngleMap.get(lookaheadTurretToTargetDistance).getRadians();
+    if (lastTurretAngle == null) lastTurretAngle = turretAngle;
+    if (Double.isNaN(lastHoodAngle)) lastHoodAngle = hoodAngle;
+    turretVelocity =
+        turretAngleFilter.calculate(
+            turretAngle.minus(lastTurretAngle).getRadians() / Constants.loopPeriodSecs);
+    hoodVelocity =
+        hoodAngleFilter.calculate((hoodAngle - lastHoodAngle) / Constants.loopPeriodSecs);
+    lastTurretAngle = turretAngle;
+    lastHoodAngle = hoodAngle;
+    latestParameters =
+        new LaunchingParameters(
+            lookaheadTurretToTargetDistance >= minDistance
+                && lookaheadTurretToTargetDistance <= maxDistance,
+            turretAngle,
+            turretVelocity,
+            hoodAngle,
+            hoodVelocity,
+            launchFlywheelSpeedMap.get(lookaheadTurretToTargetDistance));
+
+    // Log calculated values
+    Logger.recordOutput("LaunchCalculator/LookaheadPose", lookaheadPose);
+    Logger.recordOutput("LaunchCalculator/TurretToTargetDistance", lookaheadTurretToTargetDistance);
+
+    return latestParameters;
+  }
+
+  public void clearLaunchingParameters() {
+    latestParameters = null;
+  }
+}
+}
