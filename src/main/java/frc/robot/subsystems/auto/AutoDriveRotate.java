614203d06f55089941b6ed71041683a7cde24ca2
SnappleRamen <130863864+SnappleRamen@users.noreply.github.com>
2026-02-24 18:59:17 -0800

    more smoothing for testing

--- src/main/java/frc/robot/subsystems/auto/AutoDriveRotate.java
+++ src/main/java/frc/robot/subsystems/auto/AutoDriveRotate.java
@@ -54,7 +54,6 @@ public class AutoDriveRotate {
       pidRotate.setTolerance(TOLERANCE, VELOCITY_TOLERANCE);
       setName("Auto Align");
       addRequirements(drive);
-
     }
 
     // TODO: Add auto rotate for launching game pieces to corners + climb alignment
