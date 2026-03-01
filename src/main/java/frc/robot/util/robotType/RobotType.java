5cf387b57163fb45492784461906780159acf5ab
Mehvoy <mehvoy@outlook.com>
2026-02-28 18:38:14 -0800

    Changed the RobotTypesEnum to final

--- src/main/java/frc/robot/util/robotType/RobotType.java
+++ src/main/java/frc/robot/util/robotType/RobotType.java
@@ -8,7 +8,7 @@ public class RobotType {
   private static final String comp = ""; // Comp bot serial number
   private static final String alpha = "032B4B88"; // Alpha bot serial number
 
-  public static RobotTypesEnum type = RobotTypesEnum.OTHER;
+  public static final RobotTypesEnum type;
 
   static {
     if (RobotBase.isSimulation()) {
