70e25b8b16a7ea64629e97ad4a1f184d739e7ff9
Fatblabs <stuffducky@gmail.com>
2026-03-02 18:20:43 -0800

    changed comment, varialbe and removed dead code

--- src/main/java/frc/robot/subsystems/LaunchCalculator.java
+++ src/main/java/frc/robot/subsystems/LaunchCalculator.java
@@ -15,13 +15,8 @@ public class LaunchCalculator {
   public static Pose2d estimatedPose;
   public static double estimatedDist;
 
-  // private final LinearFilter ROC__target_turret_filter =
-  //     LinearFilter.movingAverage((int) (0.1 / TimedRobot.kDefaultPeriod));
-  // private final LinearFilter ROC__target_hood_filter =
-  //     LinearFilter.movingAverage((int) (0.1 / TimedRobot.kDefaultPeriod));
-
   // Turret transform
-  private static final Transform2d turretTransfom;
+  private static final Transform2d turretTransform;
   // private Rotation2d lastTurretAngle;
   // private double lastHoodAngle;
   private Rotation2d targetTurretAngle;
@@ -52,7 +47,7 @@ public class LaunchCalculator {
 
   // Static initializer
   static {
-    turretTransfom = LauncherConstants.turretTransform();
+    turretTransform = LauncherConstants.turretTransform();
     minDistance = 1.34;
     maxDistance = 5.60;
     phaseDelay = 0.03;
@@ -68,7 +63,7 @@ public class LaunchCalculator {
     ChassisSpeeds robotRelativeVelocity = robotState.getState().Speeds;
     /* This takes dX /s and dY /s, both multiplied by the estimated delta T (in this case it's the phase delay) to get the real dX dY for the Twist2d object.
     Twist 2d objects gives us a transformation result that tells us where the robot will end up (individually by each component). We then integrate the velocity of the x and
-    y components (field relative) from 0 to delta T. This gives us a delta X and delta Y, which we will then apply to the previous robot pose to get a new pose2d that
+    y components (robot relative) from 0 to delta T. This gives us a delta X and delta Y, which we will then apply to the previous robot pose to get a new pose2d that
     accurately represents the robot's position accounting in for angular velocity.
     */
     estimatedPose =
@@ -80,7 +75,7 @@ public class LaunchCalculator {
 
     // - Calculate distance from turret to target - //
     Translation2d target = AllianceUtils.getHubTranslation2d();
-    Pose2d turretPosition = estimatedPose.transformBy(turretTransfom);
+    Pose2d turretPosition = estimatedPose.transformBy(turretTransform);
     LaunchCalculator.estimatedPose = turretPosition;
     // grab distance between turret and center of hub
     double turretToTargetDistance = target.getDistance(turretPosition.getTranslation());
@@ -97,13 +92,13 @@ public class LaunchCalculator {
     double turretVelocityX =
         robotVelocity.vxMetersPerSecond
             - robotVelocity.omegaRadiansPerSecond
-                * (turretTransfom.getY() * Math.cos(robotAngle)
-                    - turretTransfom.getX() * Math.sin(robotAngle));
+                * (turretTransform.getY() * Math.cos(robotAngle)
+                    - turretTransform.getX() * Math.sin(robotAngle));
     double turretVelocityY =
         robotVelocity.vyMetersPerSecond
             + robotVelocity.omegaRadiansPerSecond
-                * (turretTransfom.getX() * Math.cos(robotAngle)
-                    - turretTransfom.getY() * Math.sin(robotAngle));
+                * (turretTransform.getX() * Math.cos(robotAngle)
+                    - turretTransform.getY() * Math.sin(robotAngle));
 
     // Account for imparted velocity by robot (turret) to offset
     double timeOfFlight;
@@ -129,23 +124,7 @@ public class LaunchCalculator {
         targetAngleFieldRelative.minus(lookaheadPose.getRotation()).rotateBy(Rotation2d.k180deg);
     // // Target hood angle
     targetHoodAngle = LauncherConstants.getHoodAngleFromDistance(lookaheadTurretToTargetDistance);
-    System.out.println(targetTurretAngle.getDegrees());
-
-    // // Set last turret angle to the currrent turret Angle and the last hood angle to current hood
-    // // angle
-    // if (lastTurretAngle == null) lastTurretAngle = targetTurretAngle;
-    // if (Double.isNaN(lastHoodAngle)) lastHoodAngle = targetHoodAngle;
-
-    // Calculate the angular velocity of the target angle. We're using filters to eliminate noise
-    // from the derivative for infinitsimal values
-    // turret_target_velocity =
-    //     ROC__target_turret_filter.calculate(
-    //         targetTurretAngle.minus(lastTurretAngle).getRadians() / TimedRobot.kDefaultPeriod);
-    // hood_target_velocity =
-    //     ROC__target_hood_filter.calculate(
-    //         (targetHoodAngle - lastHoodAngle) / TimedRobot.kDefaultPeriod);
-    // lastTurretAngle = targetTurretAngle;
-    // lastHoodAngle = targetHoodAngle;
+    // System.out.println(targetTurretAngle.getDegrees());
     finalParameters =
         new LaunchingParameters(
             lookaheadTurretToTargetDistance >= minDistance
