476b6ff5f976be72d4d22f5a1061a25d46eb9af8
FrameworkDS <194180343+RobototesProgrammers@users.noreply.github.com>
2026-03-14 10:29:01 -0700

    changed stator to 60amps

--- src/main/java/frc/robot/subsystems/launcher/Hood.java
+++ src/main/java/frc/robot/subsystems/launcher/Hood.java
@@ -46,7 +46,7 @@ public class Hood extends SubsystemBase {
   private static final double TARGET_TOLERANCE = 0.05; // tolerance in motor rotations
   public static final double VOLTAGE_MANUAL_CONTROL =
       1; // voltage/speed to control the motor for manual control
-  private static final double STATOR_CURRENT_LIMIT = 80; // stator limit in amps
+  private static final double STATOR_CURRENT_LIMIT = 60; // stator limit in amps
   // both forward and backward soft limits are in Rotor rotations
   private static final double FORWARD_SOFT_LIMIT =
       19; // LIMITED TO 19 - March 14th Physical limit
