994fbbaa7ea1258e8d38d114fe4a2c6ec33fc90d
jamesdooley4 <56771024+jamesdooley4@users.noreply.github.com>
2026-03-14 20:09:30 -0700

    Spotless fixup

--- src/main/java/frc/robot/subsystems/launcher/Hood.java
+++ src/main/java/frc/robot/subsystems/launcher/Hood.java
@@ -48,8 +48,7 @@ public class Hood extends SubsystemBase {
       1; // voltage/speed to control the motor for manual control
   private static final double STATOR_CURRENT_LIMIT = 60; // stator limit in amps
   // both forward and backward soft limits are in Rotor rotations
-  private static final double FORWARD_SOFT_LIMIT =
-      19; // LIMITED TO 19 - March 14th Physical limit
+  private static final double FORWARD_SOFT_LIMIT = 19; // LIMITED TO 19 - March 14th Physical limit
   private static final double BACKWARD_SOFT_LIMIT = -0.02; // -0.02 rotations, past zeroing point
 
   public final boolean TUNER_CONTROLLED = false; // boolean to check if tuner control is being used
