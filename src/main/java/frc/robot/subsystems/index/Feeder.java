fe6e577868a891a57fdd62566e489f9a14232d9a
Fatblabs <162062220+Fatblabs@users.noreply.github.com>
2026-03-16 19:46:36 -0700

    Issue 97 100 (#115)
    * V1 of issue 97. TODO: Make these values tunable for future tuning
    
    * IndexerSubsystem now utilizes both default velocities
    
    * Indexer subsystem is now tunable
    
    * Gemini changes
    
    * Converted intake rollers to use velocity based input
    
    * gemini
    
    * gemini
    
    * gemini
    
    * Tuned feedforward voltages
    
    * Mentor changes
    
    * formating changes
    
    * Mentor changes
    
    * aaarya requested change
    
    * revert some stuff that didn't need to be changed
    
    * redelete
    
    * aarya requested change
    
    * james changes
    
    ---------
    
    Co-authored-by: FrameworkDS <194180343+RobototesProgrammers@users.noreply.github.com>
    Co-authored-by: Jetblackdragon <64041077+Jetblackdragon@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/index/Feeder.java
+++ src/main/java/frc/robot/subsystems/index/Feeder.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.index;
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
-import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
@@ -13,23 +13,25 @@ import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Hardware;
 import frc.robot.generated.CompTunerConstants;
 import frc.robot.util.robotType.RobotType;
+import frc.robot.util.tuning.NtTunableBoolean;
+import frc.robot.util.tuning.NtTunableDouble;
 
-public class FeederSubsystem extends SubsystemBase {
-  private int ballsDetectedNum = 0;
-  public static final double FEEDER_VOLTAGE = 11;
-  private static final int feederRumbleThreshold = 67;
-  private VoltageOut voltReq = new VoltageOut(0);
+public class Feeder extends SubsystemBase {
+  private final double D_TARGET_RPS = 92;
+  private final NtTunableBoolean TUNABLE_ENABLE =
+      new NtTunableBoolean("SmartDashboard/Tunables/FeederRPS", false);
+  private final NtTunableDouble TARGET_RPS =
+      new NtTunableDouble("SmartDashboard/FeederSubsystem/TargetVelocityRPS", D_TARGET_RPS);
+  private final VelocityVoltage TARGET_VELOCITY = new VelocityVoltage(D_TARGET_RPS); // Rotations/s
 
   private final TalonFX feedMotor;
-
   private final FlywheelSim motorSim;
 
-  public FeederSubsystem() {
+  public Feeder() {
     feedMotor =
         new TalonFX(
             Hardware.FEEDER_MOTOR_ID,
@@ -51,7 +53,6 @@ public class FeederSubsystem extends SubsystemBase {
   }
 
   public void feederConfig() {
-
     TalonFXConfigurator cfg = feedMotor.getConfigurator();
     TalonFXConfiguration talonFXConfiguration = new TalonFXConfiguration();
 
@@ -69,41 +70,21 @@ public class FeederSubsystem extends SubsystemBase {
     talonFXConfiguration.CurrentLimits.SupplyCurrentLimit = 30;
     talonFXConfiguration.CurrentLimits.SupplyCurrentLimitEnable = true;
 
+    talonFXConfiguration.Slot0.kV = 11.28 / 92;
+
     cfg.apply(talonFXConfiguration);
   }
 
-  public void setVoltage(double voltage) {
-    feedMotor.setControl(voltReq.withOutput(voltage));
+  public void runVelocity() {
+    if (TUNABLE_ENABLE.get()) {
+      feedMotor.setControl(TARGET_VELOCITY.withVelocity(TARGET_RPS.get()));
+    } else {
+      feedMotor.setControl(TARGET_VELOCITY.withVelocity(D_TARGET_RPS));
+    }
   }
 
-  public Command startMotor() {
-    return runEnd(
-            () -> {
-              setVoltage(FEEDER_VOLTAGE);
-            },
-            () -> feedMotor.stopMotor())
-        .withName("Start Feeder Motor");
-  }
-
-  public Command stopMotorCommand() {
-    return runOnce(() -> feedMotor.stopMotor()).withName("Stop Feeder Motor");
-  }
-
-  public void stopMotorVoid() {
+  public void stopMotor() {
     feedMotor.stopMotor();
-  }
-
-  // PLACEHOLDER FOR SENSOR CHECK
-  public Command checkSensor() {
-    return runOnce(
-            () -> {
-              // TODO: add logic for ballNum going up after sensor triggers
-            })
-        .withName("Check Feeder Sensor");
-  }
-
-  public int getBallsDetectedNum() {
-    return ballsDetectedNum;
   }
 
   @Override
