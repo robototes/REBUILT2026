88ef0c906314f9570fe2eb75c4806c48a3a7258a
FrameworkDS <194180343+RobototesProgrammers@users.noreply.github.com>
2026-03-16 19:47:21 -0700

    spot

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -260,20 +260,21 @@ public class Controls {
             Commands.parallel(
                 s.launcherSubsystem.launcherAimCommandV2(s.drivebaseSubsystem),
                 Commands.runOnce(() -> ledsMode = LEDMode.LAUNCHING),
-
-                Commands.waitUntil(() -> s.launcherSubsystem.isAtTarget()).andThen(
-                Commands.parallel(
-                        s.indexerSubsystem.runIndexer(),
-                        Commands.runOnce(() -> ledsMode = LEDMode.LAUNCH),
-                        Commands.waitSeconds(1)
-                            .andThen(
-                                Commands.runOnce(
-                                    () ->
-                                        intakeMode =
-                                            driverController.leftTrigger().getAsBoolean()
-                                                ? IntakeMode.INTAKE
-                                                : IntakeMode.LAUNCH)))
-                    .onlyWhile(() -> s.launcherSubsystem.isAtTarget())).repeatedly()))
+                Commands.waitUntil(() -> s.launcherSubsystem.isAtTarget())
+                    .andThen(
+                        Commands.parallel(
+                                s.indexerSubsystem.runIndexer(),
+                                Commands.runOnce(() -> ledsMode = LEDMode.LAUNCH),
+                                Commands.waitSeconds(1)
+                                    .andThen(
+                                        Commands.runOnce(
+                                            () ->
+                                                intakeMode =
+                                                    driverController.leftTrigger().getAsBoolean()
+                                                        ? IntakeMode.INTAKE
+                                                        : IntakeMode.LAUNCH)))
+                            .onlyWhile(() -> s.launcherSubsystem.isAtTarget()))
+                    .repeatedly()))
         .onFalse(
             s.launcherSubsystem
                 .rawStowCommand()
