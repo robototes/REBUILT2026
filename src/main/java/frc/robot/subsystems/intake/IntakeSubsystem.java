622f9f60806f67e83fc7818520b6d435c049a285
Jetblackdragon <64041077+Jetblackdragon@users.noreply.github.com>
2026-03-16 22:40:19 -0700

    agitate rps (#130)

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -37,7 +37,7 @@ public class IntakeSubsystem extends SubsystemBase {
 
   public void intakeWhileLaunch() {
     intakePivot.setPivotPosition(IntakePivot.LAUNCH_POS);
-    runRollers();
+    intakeRollers.runRollers(intakeRollers.AGITATE_RPS);
   }
 
   public void smartIntake() {
