e2568c8135e80a15c096106c3b7d97b5ce18e5ea
koolpoolo <181432562+koolpoolo@users.noreply.github.com>
2026-03-17 19:03:47 -0700

    got rid of unnessary if block

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.swerve.SwerveDrivetrain.SwerveDriveState;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
@@ -173,23 +174,6 @@ public class VisionSubsystem extends SubsystemBase {
           injectFakePoseMT1 = Math.random() < VisionConstants.FAKE_POSE_RATE;
           injectFakePoseMT2 = Math.random() < VisionConstants.FAKE_POSE_RATE;
         }
-        if (!VisionConstants.USE_GET_STD_DEV) {
-          if (rawFiducials.length != 1) {
-            // Multi tag pose Estimation
-            processLimelight(
-                camera.getBetterPoseEstimate(),
-                rawFieldPose3dEntry,
-                injectFakePoseMT1,
-                VisionConstants.USE_GET_STD_DEV);
-          } else {
-            // Single tag pose Estimation
-            processLimelight(
-                camera.getPoseEstimateMegatag2(),
-                rawFieldPose3dEntry,
-                injectFakePoseMT2,
-                VisionConstants.USE_GET_STD_DEV);
-          }
-        } else {
           processLimelight(
               camera.getBetterPoseEstimate(),
               rawFieldPose3dEntry,
@@ -200,7 +184,6 @@ public class VisionSubsystem extends SubsystemBase {
               rawFieldPose3dEntry,
               injectFakePoseMT2,
               VisionConstants.USE_GET_STD_DEV);
-        }
       }
     }
   }
