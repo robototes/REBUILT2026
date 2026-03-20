8d2d152e6d002e5e842c6af20a56383af2f1451f
koolpoolo <181432562+koolpoolo@users.noreply.github.com>
2026-03-19 17:53:25 -0700

    removed D in turret for comp bot

--- src/main/java/frc/robot/subsystems/launcher/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/launcher/TurretSubsystem.java
@@ -47,7 +47,7 @@ public class TurretSubsystem extends SubsystemBase {
   // PID variables
   private static final double kP = RobotType.isAlpha() ? 2.97 : 500;
   private static final double kI = 0;
-  private static final double kD = RobotType.isAlpha() ? 1 : 2;
+  private static final double kD = RobotType.isAlpha() ? 1 : 0;
   private static final double kG = 0;
   private static final double kS = RobotType.isAlpha() ? 0.41 : 0.346;
   private static final double kV = 0.9;
