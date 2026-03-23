86626c21aa306813f98bb72e8e41464762c72427
Fatblabs <stuffducky@gmail.com>
2026-03-23 10:35:23 -0700

    more unused code

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -114,7 +114,6 @@ public class Controls {
     configureIndexingBindings();
     configureIntakeBindings();
     configureAutoAlignBindings();
-    configureVisionBindings();
     configureTurretBindings();
     configureLedBindings();
   }
