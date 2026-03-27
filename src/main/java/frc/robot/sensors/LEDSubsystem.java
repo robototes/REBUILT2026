3c4eda3a8c1ae6a4c5f40ad03b5a60d8e67612a8
Roxie222 <s-furlongan@bsd405.org>
2026-03-26 18:49:17 -0700

    change nt value

--- src/main/java/frc/robot/sensors/LEDSubsystem.java
+++ src/main/java/frc/robot/sensors/LEDSubsystem.java
@@ -10,6 +10,7 @@ import edu.wpi.first.networktables.StringPublisher;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -133,7 +134,7 @@ public class LEDSubsystem extends SubsystemBase {
     lastToggleTime = Timer.getFPGATimestamp();
 
     currentPatternPub.set("ALTERNATING");
-    currentColorPub.set(a.toHexString());
+    currentColorPub.set("NONE");
     alternatingColorsPub.set("A:" + a.toHexString() + " | B:" + b.toHexString());
 
     setHardwareColor(a);
