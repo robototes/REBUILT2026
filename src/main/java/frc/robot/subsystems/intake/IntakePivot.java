583380924ebca7cef1e524e730205d849665ffd7
Fatblabs <stuffducky@gmail.com>
2026-04-06 11:00:01 -0700

    Reduced auto zero voltage to something managable

--- src/main/java/frc/robot/subsystems/intake/IntakePivot.java
+++ src/main/java/frc/robot/subsystems/intake/IntakePivot.java
@@ -27,7 +27,7 @@ public class IntakePivot extends SubsystemBase {
   private final TalonFX pivotMotor;
   private final MotionMagicVoltage request = new MotionMagicVoltage(0);
   private final VoltageOut voltageRequest = new VoltageOut(0).withIgnoreSoftwareLimits(true);
-  private static final double AUTO_ZERO_VOLTAGE = 8;
+  private static final double AUTO_ZERO_VOLTAGE = 4;
   private final Timer timer = new Timer();
 
   // Positions
