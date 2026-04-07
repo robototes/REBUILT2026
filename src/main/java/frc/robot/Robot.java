8c9d16f0f731306ad816761f6f6cbfbaabefcee6
Mark Peterson <mark.peterson@live.com>
2026-04-06 17:00:05 -0700

    Update Robot.java
    Fixed capitalization to match variable name

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -212,7 +212,7 @@ public class Robot extends TimedRobot {
     CommandScheduler.getInstance()
         .cancelAll(); // Prevent auto commands from persisting past auto or during testing.
 
-    // Tell launch calculator to start a new cycle for "hasmovedatleastonce" boolean
+    // Tell launch calculator to start a new cycle for "hasMovedAtLeastOnce" boolean
     LaunchCalculator.getInstance().robotDisable();
   }
 
