17d833fbf6322dd6dd9a61ba9913f6cbce2b8b7a
FrameworkDS <194180343+RobototesProgrammers@users.noreply.github.com>
2026-04-09 16:33:36 -0700

    reduced flywheel speeds

--- src/main/java/frc/robot/util/tuning/LauncherConstants.java
+++ src/main/java/frc/robot/util/tuning/LauncherConstants.java
@@ -60,17 +60,17 @@ public class LauncherConstants {
   };
 
   private static final LauncherDistanceDataPoint[] compDistanceData = {
-    new LauncherDistanceDataPoint(1, 1.5, 40, 1.333),
-    new LauncherDistanceDataPoint(1.5, 2, 41, 0.933),
-    new LauncherDistanceDataPoint(2, 3, 40, 0.9),
-    new LauncherDistanceDataPoint(2.55, 3, 46, 1.267),
-    new LauncherDistanceDataPoint(3.2, 4.5, 48, 1.133),
-    new LauncherDistanceDataPoint(3.75, 5, 50, 1.2),
-    new LauncherDistanceDataPoint(4.2, 5, 54.5, 1.233),
-    new LauncherDistanceDataPoint(4.5, 5.5, 55, 1.3),
-    new LauncherDistanceDataPoint(5, 6.5, 57.5, 1.267),
-    new LauncherDistanceDataPoint(5.5, 7, 62, 1.267),
-    new LauncherDistanceDataPoint(8, 8, 80, 1.53)
+    new LauncherDistanceDataPoint(1, 1.5, 30, 1.333),
+    new LauncherDistanceDataPoint(1.5, 2, 31, 0.933),
+    new LauncherDistanceDataPoint(2, 3, 30, 0.9),
+    new LauncherDistanceDataPoint(2.55, 3, 36, 1.267),
+    new LauncherDistanceDataPoint(3.2, 4.5, 38, 1.133),
+    new LauncherDistanceDataPoint(3.75, 5, 40, 1.2),
+    new LauncherDistanceDataPoint(4.2, 5, 44.5, 1.233),
+    new LauncherDistanceDataPoint(4.5, 5.5, 45, 1.3),
+    new LauncherDistanceDataPoint(5, 6.5, 47.5, 1.267),
+    new LauncherDistanceDataPoint(5.5, 7, 52, 1.267),
+    new LauncherDistanceDataPoint(8, 8, 90, 1.53)
   };
 
   private static final InterpolatingDoubleTreeMap flywheelMap = new InterpolatingDoubleTreeMap();
@@ -105,7 +105,7 @@ public class LauncherConstants {
   // }
 
   public static double getFlywheelSpeedFromDistance(double distance) {
-    return flywheelMap.get(distance > 4.0 ? distance + distanceOffset : distance);
+    return flywheelMap.get(distance);
   }
 
   public static Translation2d launcherFromRobot(Pose2d robot) {
