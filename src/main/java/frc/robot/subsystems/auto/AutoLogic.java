79a190ae794fc7d513ba65e89c9307edffb16ad9
Fatblabs <stuffducky@gmail.com>
2026-04-09 08:29:48 -0700

    sim not usiing right now so 0 is a placeholder

--- src/main/java/frc/robot/subsystems/auto/AutoLogic.java
+++ src/main/java/frc/robot/subsystems/auto/AutoLogic.java
@@ -229,7 +229,7 @@ public class AutoLogic {
 
   public static Command launcherSimCommand() {
     return Commands.sequence(
-        AutoDriveRotate.autoRotate(s, () -> 0, () -> 0), // SIM PURPOSES ONLY
+        AutoDriveRotate.autoRotate(s, () -> 0, () -> 0, () -> 0), // SIM PURPOSES ONLY
         Commands.run(
                 () ->
                     FuelSim.getInstance()
