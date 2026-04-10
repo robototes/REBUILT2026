4e463a5d718a4c91436fbe5ef4986520f02e3314
DriverStation3 <194180343+RobototesProgrammers@users.noreply.github.com>
2026-04-10 11:35:06 -0700

    lower mmc

--- src/main/java/frc/robot/subsystems/launcher/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/launcher/TurretSubsystem.java
@@ -69,9 +69,9 @@ public class TurretSubsystem extends SubsystemBase {
   private static final int SUPPLY_CURRENT_LIMIT = 20; // ampsd
 
   // Motion Magic Config
-  private static final double CRUISE_VELOCITY = 200;
-  private static final double ACCELERATION = 1200;
-  private static final double JERK = 6000;
+  private static final double CRUISE_VELOCITY = 1;
+  private static final double ACCELERATION = 1.2;
+  private static final double JERK = 2;
 
   // Gear Ratio
   private static final double GEAR_RATIO = RobotType.isAlpha() ? 24 : 72;
