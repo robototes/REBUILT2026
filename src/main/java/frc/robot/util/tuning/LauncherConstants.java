ceb2878d581be0699602099eef66028dfe2886dc
SnappyRiffs <jadonjung3@gmail.com>
2026-04-30 09:15:00 -0700

    more fugding

--- src/main/java/frc/robot/util/tuning/LauncherConstants.java
+++ src/main/java/frc/robot/util/tuning/LauncherConstants.java
@@ -62,12 +62,12 @@ public class LauncherConstants {
 
   private static final LauncherDistanceDataPoint[] compDistanceData = {
     new LauncherDistanceDataPoint(1, 1.5, 40, 1.018),
-    new LauncherDistanceDataPoint(1.5, 2.2, 45, 1.138),
-    new LauncherDistanceDataPoint(2, 3.4, 48, 1.104),
-    new LauncherDistanceDataPoint(2.55, 3.5, 50, 1.204),
-    new LauncherDistanceDataPoint(3.2, 4.5, 52, 1.15),
-    new LauncherDistanceDataPoint(3.5, 4.8, 55, 1.229),
-    new LauncherDistanceDataPoint(3.75, 5, 55.5, 1.217),
+    new LauncherDistanceDataPoint(1.5, 2.2, 43, 1.138),
+    new LauncherDistanceDataPoint(2, 3.4, 46, 1.104),
+    new LauncherDistanceDataPoint(2.55, 3.5, 48, 1.204),
+    new LauncherDistanceDataPoint(3.2, 4.5, 51, 1.15),
+    new LauncherDistanceDataPoint(3.5, 4.8, 54, 1.229),
+    new LauncherDistanceDataPoint(3.75, 5, 54.5, 1.217),
     new LauncherDistanceDataPoint(4.2, 5.125, 57.5, 1.275),
     new LauncherDistanceDataPoint(4.5, 5.5, 62, 1.305),
     new LauncherDistanceDataPoint(5, 5.8, 64, 1.349),
