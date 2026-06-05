e36fd4128cd6b5cc8d86f8d2ad0e94e621601998
FrameworkDS <194180343+RobototesProgrammers@users.noreply.github.com>
2026-06-04 20:31:14 -0700

    retuned launcher map

--- src/main/java/frc/robot/util/tuning/LauncherConstants.java
+++ src/main/java/frc/robot/util/tuning/LauncherConstants.java
@@ -61,17 +61,17 @@ public class LauncherConstants {
   };
 
   private static final LauncherDistanceDataPoint[] compDistanceData = {
-    new LauncherDistanceDataPoint(1, 1.5, 40, 1.018),
-    new LauncherDistanceDataPoint(1.5, 2.2, 43, 1.138),
-    new LauncherDistanceDataPoint(2, 3.4, 46, 1.104),
-    new LauncherDistanceDataPoint(2.55, 3.5, 48, 1.204),
-    new LauncherDistanceDataPoint(3.2, 4.5, 51, 1.15),
-    new LauncherDistanceDataPoint(3.5, 4.8, 54, 1.189),
-    new LauncherDistanceDataPoint(3.75, 5, 54.5, 1.197),
-    new LauncherDistanceDataPoint(4.2, 5.125, 57.5, 1.23), // measured tof
-    new LauncherDistanceDataPoint(4.5, 5.5, 60, 1.265),
-    new LauncherDistanceDataPoint(5, 5.8, 64, 1.289),
-    new LauncherDistanceDataPoint(5.8, 6.6, 67, 1.338),
+    new LauncherDistanceDataPoint(1, 1.5, 35, 1.018),
+    new LauncherDistanceDataPoint(1.5, 2.2, 38, 1.138),
+    new LauncherDistanceDataPoint(2, 3.4, 38, 1.104),
+    new LauncherDistanceDataPoint(2.55, 3.5, 45, 1.204),
+    new LauncherDistanceDataPoint(3.2, 4.5, 47, 1.15),
+    new LauncherDistanceDataPoint(3.5, 4.8, 49, 1.189),
+    new LauncherDistanceDataPoint(3.75, 5, 49, 1.197),
+    new LauncherDistanceDataPoint(4.2, 5.125, 52.5, 1.23), // measured tof
+    new LauncherDistanceDataPoint(4.5, 5.5, 55, 1.265),
+    new LauncherDistanceDataPoint(5, 5.8, 62, 1.289),
+    new LauncherDistanceDataPoint(5.8, 6.6, 65, 1.338),
     new LauncherDistanceDataPoint(8, 9, 90, 1.53)
   };
 
